5cb1b904113b14d5ff6f6d8a5e96d815fa9e5d80
Jenkins <jenkins@ubuntu.myguest.virtualbox.org>
2026-06-24 21:48:07 +0300

    Add Dockerfile, build.sh

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM aflplusplus/aflplusplus:latest
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    make \
+    gcc \
+    g++ \
+    autoconf \
+    automake \
+    libtool \
+    pkg-config \
+    zlib1g-dev \
+    libpng-dev \
+    libjpeg-dev \
+    libtiff-dev \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+echo "Cleaning"
+
+make clean || true
+
+echo "Configure"
+
+./configure || true
+
+echo "Release build"
+
+make CFLAGS="-O2"
+
+cp sam2p sam2p-release
+
+echo "Debug build"
+
+make clean
+
+make CFLAGS="-g -O0"
+
+cp sam2p sam2p-debug
+
+echo "Instrumented build"
+
+make clean
+
+CC=afl-clang-fast \
+CXX=afl-clang-fast++ \
+CFLAGS="-fsanitize=address -g" \
+make
+
+cp sam2p sam2p-instrumented
