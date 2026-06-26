351e719b9433b0773dbe6ee285d41284c71d54db
soft1it <ouinq05@gmail.com>
2026-06-26 21:39:31 +0000

    ///

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,16 +6,16 @@ echo "===  sam2p-instrumented START ==="
 
 mkdir -p results
 
-./sam2p-instrumented test.bmp PNG:results/out1.png
+./sam2p-coverage test.bmp PNG:results/out1.png
 echo "test.bmp -> PNG"
 
-./sam2p-instrumented test.png JPEG:results/out2.jpg
+./sam2p-coverage test.png JPEG:results/out2.jpg
 echo "test.png -> JPEG"
 
-./sam2p-instrumented test.jpg TIFF:results/out3.tiff
+./sam2p-coverage test.jpg TIFF:results/out3.tiff
 echo "test.jpg -> TIFF"
 
-./sam2p-instrumented test.ppm BMP:results/out4.bmp
+./sam2p-coveragr test.ppm BMP:results/out4.bmp
 echo "test.pnm -> BMP"
 
 echo ""
